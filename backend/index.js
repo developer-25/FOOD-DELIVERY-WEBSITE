55c3d2d791f84d2f11e07cd085b28e7ddbc0d9af
developer-25 <kalashmantri2002@gmail.com>
2024-08-08 10:43:53 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const cors = require('cors');
 require('dotenv').config()
-
-
 const app = express()
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}))
+
+
 const port = process.env.PORT|| 5000;
 const mongoDB=require("./db")
 
