36333dff60349a6c59c5e01b5f4a836856ba59e9
developer-25 <kalashmantri2002@gmail.com>
2024-08-12 00:29:38 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express()
 
 const cors = require('cors');
 
-const allowedOrigins = process.env.ALLOWED_ORIGIN || "http://localhost:3000";
+const allowedOrigins = process.env.ALLOWED_ORIGIN || "https://goeatfood.netlify.app/login"||"https://goeatfood.netlify.app/CreateUser";
 
 app.use(cors({
   origin: allowedOrigins,
