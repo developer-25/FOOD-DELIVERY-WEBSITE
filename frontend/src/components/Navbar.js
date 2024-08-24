a0679e5d8579f88fab077e87c31bc0002e6d1cae
developer-25 <kalashmantri2002@gmail.com>
2024-08-24 16:54:16 +0530

    commit

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -16,7 +16,17 @@ export default function Navbar() {
     navigate("/");
   };
 
-  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+  const handleNavCollapse = () => {
+    setIsNavCollapsed(!isNavCollapsed);
+    const navbarToggle = document.getElementById('navbarNav');
+    if (!isNavCollapsed) {
+      // Close the navbar
+      navbarToggle.classList.remove('show');
+    } else {
+      // Open the navbar
+      navbarToggle.classList.add('show');
+    }
+  };
 
   return (
     <div>
@@ -35,7 +45,7 @@ export default function Navbar() {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarNav">
+          <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav me-auto mb-2">
               <li className="nav-item">
                 <Link className="nav-link active fs-5" aria-current="page" to="/">Home</Link>
